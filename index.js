6976e86e2ecb1aaa320cf709952689ab221d02c1
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-22 00:45:01 -0500

    Updating index.html and index.js file

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var dhcp = require('./dhcp')
+var fs = require('fs');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket){  
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  setInterval(function htmlOutputFile() {
+  	fs.readFile(dhcp.htmlOutputFile, 'utf8', function (err, data) {
+	  if (err) throw err;	  
+	  io.emit("refreshContent", data);
+	});	
+  }, 3007);
+  setInterval(function dhcpLeases(){
+  	//var data = dhcp.replaceHtmlContent();
+  	var data = ">> " + Math.random();
+  	io.emit("refreshTimes", data);  	
+  }, 2991);
 });
 
 http.listen(3000, function(){
